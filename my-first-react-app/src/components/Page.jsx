dcd42779a5732c56a44d98abdcd711ec221d3898
Siniiez <siniiez@Siniiezs-MacBook-Air.local>
2023-08-03 09:19:55 +0700

    feat: add vite

---
+++ my-first-react-app/src/components/Page.jsx
@@ -0,0 +1,11 @@
+function Page() {
+    return(
+    <div className="header">
+        <img src=""></img>
+        <div className="firstName">Pakorn</div>
+        <div className="lastName">Semapongpun</div>
+    </div>
+    );
+};
+
+export default Page;
